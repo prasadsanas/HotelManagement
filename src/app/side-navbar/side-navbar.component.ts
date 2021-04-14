94118f4c7ac3e8c41b8bcef95868533af71960d7
Sanas <prasad-dattaram.sanas@capgemini.com>
2021-04-15 02:38:20 +0530

    side navbar modification

--- src/app/side-navbar/side-navbar.component.ts
+++ src/app/side-navbar/side-navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-side-navbar',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SideNavbarComponent implements OnInit {
   isAdminLoggedin: boolean = true;
-  constructor() {}
+  constructor(private router: Router) {}
 
   ngOnInit(): void {}
+
+  logout() {
+    this.isAdminLoggedin = false;
+    this.router.navigate(['/login-admin']);
+  }
 }
